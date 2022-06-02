5a508c53776e2c5802264364d716f06cbdb19dd3
Maria Luíza <102473250+marimalu@users.noreply.github.com>
2022-06-02 10:54:52 -0300

    Update home.php

--- paginas/home.php
+++ paginas/home.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livro Mania - Página Inicial</title>
+    <title>Livro Favorito - Página Inicial</title>
 </head>
 <body>
-    <h1>Bem-vindo ao Livro Mania</h1>
+    <h1>Bem-vindo ao Livro Favorito</h1>
     <a href="/paginas/register.php">Registrar seu livro favorito</a>
     <a href="/paginas/about.php">Sobre nós</a>
 </body>
